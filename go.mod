ea23f1af117f49ba457606b5dffb72667a9a25ed
Brainsoft-Raxat <hamltov31@gmail.com>
2021-10-25 12:51:09 +0600

    flip: used FlipString function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Brainsoft-Raxat/onelab-hw2-exe
+
+go 1.16
+
+require github.com/Brainsoft-Raxat/onelab-hw2-lib v1.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	testlibrary "github.com/Brainsoft-Raxat/onelab-hw2-lib"
+)
+
+func main() {
+	fmt.Println(testlibrary.FlipString("AbAbAbA"))
+	fmt.Println(testlibrary.FlipString("aZ123zA"))
+}
